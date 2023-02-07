38948fbbe9417ef758ccd2528dce7806565561aa
Eloisa <53000607+Eloise1988@users.noreply.github.com>
2023-02-07 11:51:09 -0600

    Update memory.py

--- asyncV2/memory.py
+++ asyncV2/memory.py
@@ -12,6 +12,10 @@ async def get_channel_messages(chat_id, msg_id):
     api_id = 'xxxxxxxx'
     api_hash = 'xxxxxxxxxxxxxx'
     session_hash='0KSDgh9xxxxxxxxxxxxxxxxxx-xxxxxxxxxxxxxxxxxx1dfsHNVugzasdHRShvewqWla889BH='
+    # Identify your bot with his ID number, can be found using this link
+    my_bot_id = 'xxxxxxxxx_my-bot-id-number_xxxxxxxx'
+    # Storing max 3 past messages
+    max_memory_message = 3
 
     chat_id = int(chat_id)
     data = {}
@@ -32,6 +36,7 @@ async def get_channel_messages(chat_id, msg_id):
                             replied = x.reply_to.reply_to_msg_id
                         except:
                             replied = x.reply_to
+                        # The code stores the following information for each message in a dictionary: message id, date, user id of the sender, the message content, id of the message it was replied to, and if the message was pinned.    
                         data[str(x.id)] = [x.id, int(x.date.timestamp()), x.from_id.user_id, x.text, replied, x.pinned]
             except:
                 print(x.text)
@@ -45,7 +50,7 @@ async def get_channel_messages(chat_id, msg_id):
                 my_dict.append([reply_number, data[str(reply_number)][2]])
                 reply_number = data[str(reply_number)][4]
                 #Storing max 3 past messages
-                if len(my_dict) > 4:
+                if len(my_dict) > max_memory_message + 1:
                     break
         except Exception as e:
             print(e)
@@ -54,7 +59,7 @@ async def get_channel_messages(chat_id, msg_id):
         if len(my_dict) > 1:
             #Building message history/memory
             for i in range(len(my_dict) - 1, -1, -1):
-                if str(my_dict[i][1]) == 'xxxxxxxxx_my-bot-id-number_xxxxxxxx':
+                if str(my_dict[i][1]) == my_bot_id:
                     #If message comes from bot -> message is treated as response from person A
                     write_history += "A: " + data[str(my_dict[i][0])][3] + "\n"
                 else:
